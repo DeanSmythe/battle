ee59f4c5e935f75d38efd5d1e76db1f86ffe0c7c
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-28 16:37:39 +0100

    Test Helpers

--- spec/features/feature_names_spec.rb
+++ spec/features/feature_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Can add names' do
   scenario 'fill in name' do
-    visit('/')
-    fill_in 'player1_name', with: 'John'
-    fill_in 'player2_name', with: 'Dave'
-    click_button('Submit')
+    sign_in_and_play
     save_and_open_page
     expect(page).to have_content 'John'
     expect(page).to have_content 'Dave'
